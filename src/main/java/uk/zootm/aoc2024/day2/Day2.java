5360f341ce47a918e4377abd9f9c08562d6cc33b
caluml <calumleslie@gmail.com>
2024-12-02 16:09:38 +0000

    day2: Part 2
    Less happy with this one; special casing the 0 index feels icky. But it
    works!

--- src/main/java/uk/zootm/aoc2024/day2/Day2.java
+++ src/main/java/uk/zootm/aoc2024/day2/Day2.java
@@ -4,7 +4,9 @@ import com.google.common.io.Resources;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Day2 {
     public static void main(String[] args) throws IOException {
@@ -16,9 +18,46 @@ public class Day2 {
                 .count();
 
         System.out.printf("Part 1: %d%n", numSafe);
+
+        var numSafeDampened = input.stream()
+                .map(Day2::parseReport)
+                .filter(Day2::isSafeDampened)
+                .count();
+
+        System.out.printf("Part 2: %d%n", numSafeDampened);
+    }
+
+    static boolean isSafeDampened(long[] report) {
+        int problematicLevel = firstProblematicLevel(report);
+        if (problematicLevel == -1) {
+            return true;
+        }
+
+        // We know it's this transition which is bad so we can try removing the level before or after
+
+        List<long[]> toCheck = new ArrayList<>();
+        toCheck.add(removeIndex(report, problematicLevel));
+        toCheck.add(removeIndex(report, problematicLevel + 1));
+        if(problematicLevel == 1) {
+            // Special case: We may have just decided the direction was wrong due to the transition before so cram 0
+            // in as well
+            toCheck.add(removeIndex(report, 0));
+        }
+        return toCheck.stream().anyMatch(Day2::isSafe);
+    }
+
+    static long[] removeIndex(long[] report, int index) {
+        long[] newReport = new long[report.length - 1];
+        System.arraycopy(report, 0, newReport, 0, index);
+        System.arraycopy(report, index + 1, newReport, index, report.length - index - 1);
+        return newReport;
     }
 
     static boolean isSafe(long[] report) {
+        return firstProblematicLevel(report) == -1;
+    }
+
+    private static int firstProblematicLevel(long[] report) {
         int signum = 0;
         for (int i = 0; i < report.length - 1; i++) {
             long difference = report[i + 1] - report[i];
@@ -29,19 +68,19 @@ public class Day2 {
             }
 
             if (difference == 0) {
-                return false;
+                return i;
             }
 
             if (diffSignum != signum) {
                 // Changed direction
-                return false;
+                return i;
             }
 
             if (Math.abs(difference) > 3) {
-                return false;
+                return i;
             }
         }
-        return true;
+        return -1;
     }
 
     public static long[] parseReport(String report) {
--- src/test/java/uk/zootm/aoc2024/day2/Day2Test.java
+++ src/test/java/uk/zootm/aoc2024/day2/Day2Test.java
@@ -7,13 +7,35 @@ import static org.assertj.core.api.BDDAssertions.then;
 public class Day2Test {
 
     @Test
-    public void exampleReports() {
+    public void part1_exampleReports() {
         assertSafe("7 6 4 2 1");
         assertUnsafe("1 2 7 8 9");
         assertUnsafe("9 7 6 2 1");
         assertUnsafe("1 3 2 4 5");
         assertUnsafe("8 6 4 4 1");
         assertSafe("1 3 6 7 9");
+    }
+
+    @Test
+    public void part2_exampleReports() {
+        assertSafeDampened("7 6 4 2 1");
+        assertUnsafeDampened("1 2 7 8 9");
+        assertUnsafeDampened("9 7 6 2 1");
+        assertSafeDampened("1 3 2 4 5");
+        assertSafeDampened("8 6 4 4 1");
+        assertSafeDampened("1 3 6 7 9");
+    }
+
+
+    @Test
+    public void part2_myOwnExamples() {
+        assertSafeDampened("48 46 47 49 51 54 56");
+    }
+
+    @Test
+    public void removeIndex() {
+        long[] removed = Day2.removeIndex(new long[]{0, 1, 2, 3, 4, 5}, 2);
+        then(removed).containsExactly(0, 1, 3, 4, 5);
     }
 
     private void assertSafe(String reportString) {
@@ -24,5 +46,15 @@ public class Day2Test {
     private void assertUnsafe(String reportString) {
         var report = Day2.parseReport(reportString);
         then(Day2.isSafe(report)).isFalse().describedAs("Should be unsafe: [%s]", reportString);
+    }
+
+    private void assertSafeDampened(String reportString) {
+        var report = Day2.parseReport(reportString);
+        then(Day2.isSafeDampened(report)).isTrue().describedAs("Should be safe: [%s]", reportString);
+    }
+
+    private void assertUnsafeDampened(String reportString) {
+        var report = Day2.parseReport(reportString);
+        then(Day2.isSafeDampened(report)).isFalse().describedAs("Should be unsafe: [%s]", reportString);
     }
 }
