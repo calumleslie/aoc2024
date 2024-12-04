fb9957d607e28a4d7a2cf4c69016753e7f9f7976
caluml <calumleslie@gmail.com>
2024-12-04 10:56:26 +0000

    day4: Part 1
    A lovely generalized grid abstraction which I'm sure part 2 will fully
    destroy.

---
+++ src/main/java/uk/zootm/aoc2024/day4/Day4.java
@@ -0,0 +1,121 @@
+package uk.zootm.aoc2024.day4;
+
+import com.google.common.base.Preconditions;
+import com.google.common.io.Resources;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Day4 {
+
+    public static void main(String[] args) throws IOException {
+        var input = Resources.readLines(Day4.class.getResource("input"), StandardCharsets.UTF_8);
+        var grid = new Grid(input);
+
+        System.out.printf("Part 1: %d%n", grid.find("XMAS").count());
+    }
+
+    enum Direction {
+        N(0, -1),
+        NE(1, -1),
+        E(1, 0),
+        SE(1, 1),
+        S(0, 1),
+        SW(-1, 1),
+        W(-1, 0),
+        NW(-1, -1);
+
+        private final Vector vector;
+
+        private Direction(int x, int y) {
+            Preconditions.checkArgument(x >= -1 && x <= 1);
+            Preconditions.checkArgument(y >= -1 && y <= 1);
+            this.vector = new Vector(x, y);
+        }
+
+        public Vector vector() {
+            return vector;
+        }
+    }
+
+    record Vector(int x, int y) {
+        Vector plus(Vector other) {
+            return new Vector(x + other.x, y + other.y);
+        }
+
+        Vector times(int magnitude) {
+            return new Vector(x * magnitude, y * magnitude);
+        }
+    }
+
+    record FindResult(Vector location, Direction direction) {
+    }
+
+    record Grid(List<String> lines) {
+        Grid {
+            long distinctLengths = lines.stream().map(String::length).distinct().count();
+            Preconditions.checkArgument(distinctLengths == 1L);
+        }
+
+        Stream<FindResult> find(String target) {
+            var possibleResults = coords().flatMap(loc -> Stream.of(Direction.values()).map(dir -> new FindResult(loc, dir)));
+
+            return possibleResults.filter(result -> matches(result.location(), result.direction(), target));
+        }
+
+        Stream<Vector> coords() {
+            return IntStream.range(0, height())
+                    .mapToObj(y -> IntStream.range(0, width()).mapToObj(x -> new Vector(x, y)))
+                    .flatMap(x -> x);
+        }
+
+        boolean matches(Vector coord, Direction direction, String target) {
+            checkBounds(coord);
+
+            Vector end = coord.plus(direction.vector().times(target.length() - 1));
+            if (!inBounds(end)) {
+                return false;
+            }
+
+            IntStream actualCharacters = Stream.iterate(coord, c -> c.plus(direction.vector())).mapToInt(this::charAt);
+            IntStream targetCharacters = target.chars();
+
+            var targetIterator = targetCharacters.iterator();
+            var actualIterator = actualCharacters.iterator();
+
+            while (targetIterator.hasNext()) {
+                // Don't need to check actual due to inBounds check above
+                if (targetIterator.nextInt() != actualIterator.nextInt()) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        char charAt(Vector coord) {
+            checkBounds(coord);
+            // Not very unicode compliant right now
+            return lines.get(coord.y()).charAt(coord.x());
+        }
+
+        private void checkBounds(Vector coord) {
+            Preconditions.checkArgument(inBounds(coord), "Out of bounds: %s", coord);
+        }
+
+        int width() {
+            return lines.getFirst().length();
+        }
+
+        int height() {
+            return lines.size();
+        }
+
+        boolean inBounds(Vector coord) {
+            return coord.x() >= 0 && coord.x() < width() && coord.y() >= 0 && coord.y() < height();
+        }
+    }
+}
---
+++ src/test/java/uk/zootm/aoc2024/day4/Day4Test.java
@@ -0,0 +1,90 @@
+package uk.zootm.aoc2024.day4;
+
+import org.junit.jupiter.api.Test;
+import uk.zootm.aoc2024.day4.Day4.Direction;
+import uk.zootm.aoc2024.day4.Day4.FindResult;
+import uk.zootm.aoc2024.day4.Day4.Grid;
+import uk.zootm.aoc2024.day4.Day4.Vector;
+
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Day4Test {
+    @Test
+    public void matches_variousMatches() {
+        Grid grid = gridOf("""
+                SATOR
+                AREPO
+                TENET
+                OPERA
+                ROTAS
+                """);
+
+        assertThat(grid.matches(new Vector(0, 0), Direction.S, "SATOR")).isTrue();
+        assertThat(grid.matches(new Vector(0, 0), Direction.E, "SATOR")).isTrue();
+        assertThat(grid.matches(new Vector(4, 4), Direction.N, "SATOR")).isTrue();
+        assertThat(grid.matches(new Vector(4, 4), Direction.W, "SATOR")).isTrue();
+        assertThat(grid.matches(new Vector(4, 4), Direction.NW, "SRNRS")).isTrue();
+    }
+
+    @Test
+    public void matches_outOfBounds() {
+        Grid grid = gridOf("""
+                SATOR
+                AREPO
+                TENET
+                OPERA
+                ROTAS
+                """);
+
+        assertThat(grid.matches(new Vector(0, 0), Direction.E, "SATORTENET")).isFalse();
+        assertThat(grid.matches(new Vector(4, 4), Direction.E, "SATOR")).isFalse();
+        assertThat(grid.matches(new Vector(0, 0), Direction.N, "SATOR")).isFalse();
+    }
+
+    @Test
+    public void find_multipleMatches() {
+        Grid grid = gridOf("""
+                SATOR
+                AREPO
+                TENET
+                OPERA
+                ROTAS
+                """);
+
+        assertThat(grid.find("AREPO")).containsExactlyInAnyOrder(
+                new FindResult(new Vector(1, 0), Direction.S),
+                new FindResult(new Vector(0, 1), Direction.E),
+                new FindResult(new Vector(4, 3), Direction.W),
+                new FindResult(new Vector(3, 4), Direction.N));
+
+        assertThat(grid.find("OO")).containsExactlyInAnyOrder(
+                new FindResult(new Vector(3, 0), Direction.SE),
+                new FindResult(new Vector(4, 1), Direction.NW),
+                new FindResult(new Vector(0, 3), Direction.SE),
+                new FindResult(new Vector(1, 4), Direction.NW));
+    }
+
+    @Test
+    public void find_example() {
+        Grid grid = gridOf("""
+                MMMSXXMASM
+                MSAMXMSMSA
+                AMXSXMAAMM
+                MSAMASMSMX
+                XMASAMXAMM
+                XXAMMXXAMA
+                SMSMSASXSS
+                SAXAMASAAA
+                MAMMMXMMMM
+                MXMXAXMASX
+                """);
+
+        assertThat(grid.find("XMAS")).hasSize(18);
+    }
+
+    private Grid gridOf(String string) {
+        return new Grid(string.strip().lines().collect(Collectors.toUnmodifiableList()));
+    }
+}
