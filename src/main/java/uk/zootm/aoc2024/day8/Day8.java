1c8b9f7b20e675833827fcfb081e99ed36e29e63
caluml <calumleslie@gmail.com>
2024-12-10 12:48:03 +0000

    day8: Quick refactor to separate paths out

--- src/main/java/uk/zootm/aoc2024/day8/Day8.java
+++ src/main/java/uk/zootm/aoc2024/day8/Day8.java
@@ -15,8 +15,7 @@ public class Day8 {
         var input = Resources.readLines(Day8.class.getResource("input"), StandardCharsets.UTF_8);
         var map = AntennaMap.parse(Grid.fromCharacterGrid(input));
 
-        System.out.printf(
-                "Part 1: %d%n", map.findAllInterferingPoints().distinct().count());
+        System.out.printf("Part 1: %d%n", map.interferingPoints().distinct().count());
     }
 
     record AntennaMap(ListMultimap<String, Vector> antennas, int height, int width) {
@@ -38,32 +37,47 @@ public class Day8 {
             return new AntennaMap(result.build(), grid.width(), grid.height());
         }
 
-        Stream<Vector> findAllInterferingPoints() {
-            return antennas.asMap().values().stream()
-                    .flatMap(antennae -> findInterferingPointsFor((List<Vector>) antennae));
+        Stream<Vector> interferingPoints() {
+            return findAllPaths()
+                    .filter(PathElement::interferes)
+                    .map(PathElement::point)
+                    .distinct();
         }
 
-        Stream<Vector> findInterferingPointsFor(List<Vector> antennae) {
-            Stream<Vector> result = Stream.empty();
+        Stream<PathElement> findAllPaths() {
+            return antennas.asMap().values().stream()
+                    .flatMap(antennae -> findAllPathsForFrequency((List<Vector>) antennae));
+        }
+
+        Stream<PathElement> findAllPathsForFrequency(List<Vector> antennae) {
+            Stream<PathElement> result = Stream.empty();
             for (int i = 0; i < (antennae.size() - 1); i++) {
                 for (int j = i + 1; j < antennae.size(); j++) {
-                    result = Stream.concat(result, findInterferingPoints(antennae.get(i), antennae.get(j)));
+                    result = Stream.concat(result, findPath(antennae.get(i), antennae.get(j)));
                 }
             }
             return result;
         }
 
-        Stream<Vector> findInterferingPoints(Vector first, Vector second) {
+        Stream<PathElement> findPath(Vector first, Vector second) {
             var line = first.minus(second).minimize();
 
             var up = Stream.iterate(first, value -> value.minus(line)).takeWhile(this::inBounds);
-            var down = Stream.iterate(first, value -> value.plus(line)).takeWhile(this::inBounds);
+            var down =
+                    Stream.iterate(first.plus(line), value -> value.plus(line)).takeWhile(this::inBounds);
 
-            return Stream.concat(up, down).filter(point -> interferes(first, second, point));
+            return Stream.concat(up, down).map(point -> new PathElement(first, second, point));
         }
 
         boolean inBounds(Vector vector) {
             return vector.x() >= 0 && vector.x() < width && vector.y() >= 0 && vector.y() < height;
+        }
+    }
+
+    record PathElement(Vector antenna1, Vector antenna2, Vector point) {
+
+        boolean interferes() {
+            return Day8.interferes(antenna1, antenna2, point);
         }
     }
 
