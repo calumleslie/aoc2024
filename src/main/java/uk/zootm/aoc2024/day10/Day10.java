c9151b701a8bb5116e916e16b73878713cd1c359
caluml <calumleslie@gmail.com>
2024-12-11 13:43:45 +0000

    day10: Part 2
    Relatively easy to generalize this; the reachability becomes the count
    of routs to each peak.

--- src/main/java/uk/zootm/aoc2024/day10/Day10.java
+++ src/main/java/uk/zootm/aoc2024/day10/Day10.java
@@ -1,15 +1,17 @@
 package uk.zootm.aoc2024.day10;
 
 import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableMultiset;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Multimap;
+import com.google.common.collect.Multiset;
 import com.google.common.io.Resources;
 import java.nio.charset.StandardCharsets;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import uk.zootm.aoc2024.grid.CharacterGrid;
 import uk.zootm.aoc2024.grid.Direction;
+import uk.zootm.aoc2024.grid.Grid;
 import uk.zootm.aoc2024.grid.LongGrid;
 import uk.zootm.aoc2024.grid.ObjGrid;
 import uk.zootm.aoc2024.grid.Vector;
@@ -22,14 +24,23 @@ public class Day10 {
         var input = Resources.readLines(Day10.class.getResource("input"), StandardCharsets.UTF_8);
         var map = LongGrid.fromStringGrid(CharacterGrid.fromCharacterGrid(input));
 
-        System.out.printf("Part 1: %d%n", trailheadScore(map));
+        System.out.printf("Part 1: %d%n", peaksTrailheadScore(map));
+        System.out.printf("Part 2: %d%n", routesTrailheadScore(map));
     }
 
-    static long trailheadScore(LongGrid trailMap) {
+    static long peaksTrailheadScore(LongGrid trailMap) {
         var reachablePeaks = reachablePeaks(trailMap);
         return trailheads(trailMap)
                 .map(reachablePeaks::get)
-                .mapToLong(Set::size)
+                .mapToLong(peaks -> peaks.elementSet().size())
+                .sum();
+    }
+
+    static long routesTrailheadScore(LongGrid trailMap) {
+        var reachablePeaks = reachablePeaks(trailMap);
+        return trailheads(trailMap)
+                .map(reachablePeaks::get)
+                .mapToLong(Multiset::size)
                 .sum();
     }
 
@@ -37,17 +48,21 @@ public class Day10 {
         return trailMap.coords().filter(c -> trailMap.getAsLong(c) == 0L);
     }
 
-    static ObjGrid<Set<Vector>> reachablePeaks(LongGrid trailMap) {
+    /**
+     * Returns a grid where each cell contains a multiset of which peaks are reachable by a trail from that cell, with
+     * the count being the number of unique trails.
+     */
+    static Grid<Multiset<Vector>> reachablePeaks(LongGrid trailMap) {
         Multimap<Long, Vector> heightToLocations = HashMultimap.create();
 
         trailMap.coords().forEach(coord -> {
             heightToLocations.put(trailMap.get(coord), coord);
         });
 
-        ObjGrid<Set<Vector>> reachablePeaks = ObjGrid.empty(trailMap.width(), trailMap.height());
+        ObjGrid<Multiset<Vector>> reachablePeaks = ObjGrid.empty(trailMap.width(), trailMap.height());
 
         // Calling a "9-height" area a "peak". Each of these are reachable from themselves
-        heightToLocations.get(9L).forEach(loc -> reachablePeaks.set(loc, Set.of(loc)));
+        heightToLocations.get(9L).forEach(loc -> reachablePeaks.set(loc, ImmutableMultiset.of(loc)));
 
         // Now we walk downwards counting reachable from adjacent
         for (long height = 8; height >= 0; height--) {
@@ -59,7 +74,7 @@ public class Day10 {
                 var reachable = adjacent(trailMap, location)
                         .filter(higher::contains)
                         .flatMap(adj -> reachablePeaks.get(adj).stream())
-                        .collect(Collectors.toSet());
+                        .collect(ImmutableMultiset.toImmutableMultiset());
 
                 reachablePeaks.set(location, reachable);
             }
--- src/test/java/uk/zootm/aoc2024/day10/Day10Test.java
+++ src/test/java/uk/zootm/aoc2024/day10/Day10Test.java
@@ -2,14 +2,13 @@ package uk.zootm.aoc2024.day10;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Set;
 import org.junit.jupiter.api.Test;
 import uk.zootm.aoc2024.grid.CharacterGrid;
 import uk.zootm.aoc2024.grid.LongGrid;
 
 public class Day10Test {
     @Test
-    public void trailheadScore_example() {
+    public void peaksTrailheadScore_example() {
         var map = gridOf(
                 """
                         89010123
@@ -22,11 +21,24 @@ public class Day10Test {
                         10456732
                         """);
 
-        var reachableMap = Day10.reachablePeaks(map);
-        assertThat(Day10.trailheads(map).map(reachableMap::get).mapToInt(Set::size))
-                .containsExactly(5, 6, 5, 3, 1, 3, 5, 3, 5);
+        assertThat(Day10.peaksTrailheadScore(map)).isEqualTo(36L);
+    }
 
-        assertThat(Day10.trailheadScore(map)).isEqualTo(36L);
+    @Test
+    public void routesTrailheadScore_example() {
+        var map = gridOf(
+                """
+                        89010123
+                        78121874
+                        87430965
+                        96549874
+                        45678903
+                        32019012
+                        01329801
+                        10456732
+                        """);
+
+        assertThat(Day10.routesTrailheadScore(map)).isEqualTo(81L);
     }
 
     private static LongGrid gridOf(String s) {
