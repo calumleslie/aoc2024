20d8a500c166c6a67e4a6542779396b833647e5c
caluml <calumleslie@gmail.com>
2024-12-13 14:46:55 +0000

    day12: Part 2
    I went through _so many_ bad versions of this. Silly, silly, silly.

--- src/main/java/uk/zootm/aoc2024/day12/Day12.java
+++ src/main/java/uk/zootm/aoc2024/day12/Day12.java
@@ -1,19 +1,22 @@
 package uk.zootm.aoc2024.day12;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.io.Resources;
-import uk.zootm.aoc2024.grid.CharacterGrid;
-import uk.zootm.aoc2024.grid.Direction;
-import uk.zootm.aoc2024.grid.Vector;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import uk.zootm.aoc2024.grid.CharacterGrid;
+import uk.zootm.aoc2024.grid.Direction;
+import uk.zootm.aoc2024.grid.Vector;
 
 public class Day12 {
     public static void main(String[] args) throws IOException {
@@ -21,17 +24,23 @@ public class Day12 {
         CharacterGrid grid = CharacterGrid.fromCharacterGrid(lines);
 
         var segments = findSegments(grid);
-        System.out.printf("Part 1: %d%n", segments.stream().mapToLong(Segment::score).sum());
+        System.out.printf(
+                "Part 1: %d%n", segments.stream().mapToLong(Segment::score).sum());
+        System.out.printf(
+                "Part 2: %d%n",
+                segments.stream().mapToLong(Segment::discountScore).sum());
     }
+
+    private static final Set<Direction> ALL = ImmutableSet.copyOf(Direction.values());
 
     static List<Segment> findSegments(CharacterGrid grid) {
         Set<Vector> seen = new HashSet<>();
         List<Segment> result = new ArrayList<>();
         var coordIterator = grid.coords().iterator();
 
-        while(coordIterator.hasNext()) {
+        while (coordIterator.hasNext()) {
             var loc = coordIterator.next();
-            if(seen.contains(loc)) {
+            if (seen.contains(loc)) {
                 continue;
             }
             var segment = findSegment(grid, loc);
@@ -50,8 +59,9 @@ public class Day12 {
         toCheck.add(loc);
 
         Vector current;
-        while((current = toCheck.pollFirst()) != null) {
-            neighbours(current)
+        while ((current = toCheck.pollFirst()) != null) {
+            neighbours(current, Direction.cardinal())
+                    .map(Neighbour::location)
                     .filter(grid::inBounds)
                     .filter(neighbour -> grid.get(neighbour).equals(element))
                     // Adds to the cells list as a "side effect" of filtering out seen cells
@@ -62,8 +72,14 @@ public class Day12 {
         return new Segment(element, cells);
     }
 
-    private static Stream<Vector> neighbours(Vector vector) {
-        return Direction.cardinal().stream().map(vector::plus);
+    private static Stream<Neighbour> neighbours(Vector vector, Set<Direction> directions) {
+        return directions.stream().map(dir -> new Neighbour(vector, dir));
+    }
+
+    record Neighbour(Vector origin, Direction direction) {
+        Vector location() {
+            return origin.plus(direction);
+        }
     }
 
     record Segment(String element, Set<Vector> cells) {
@@ -79,8 +95,66 @@ public class Day12 {
             return area() * perimeter();
         }
 
+        long discountScore() {
+            return area() * cornerLocations().count();
+        }
+
+        Stream<Vector> cornerLocations() {
+            return cells.stream().flatMap(this::cornerLocations);
+        }
+
         private long edges(Vector vector) {
-            return neighbours(vector).filter(neighbour -> !cells.contains(neighbour)).count();
+            return externalNeighbours(vector, Direction.cardinal()).count();
+        }
+
+        private Stream<Vector> cornerLocations(Vector vector) {
+            return corners(vector).map(corner -> corner.location(vector));
+        }
+
+        @VisibleForTesting
+        Stream<Corner> corners(Vector vector) {
+            Set<Direction> edgeDirections =
+                    externalNeighbours(vector, ALL).map(Neighbour::direction).collect(Collectors.toSet());
+
+            return Arrays.stream(Corner.values()).filter(corner -> corner.matches(edgeDirections));
+        }
+
+        private Stream<Neighbour> externalNeighbours(Vector vector, Set<Direction> directions) {
+            return neighbours(vector, directions).filter(neighbour -> !cells.contains(neighbour.location()));
+        }
+    }
+
+    enum Corner {
+        NW(Direction.NW, 0, 0),
+        NE(Direction.NE, 1, 0),
+        SW(Direction.SW, 0, 1),
+        SE(Direction.SE, 1, 1);
+
+        private final Direction direction;
+        private final Direction anticlockwise;
+        private final Direction clockwise;
+        private final Vector offset;
+
+        Corner(Direction direction, int offsetX, int offsetY) {
+            this.direction = direction;
+            this.anticlockwise = direction.rotateSteps(-1);
+            this.clockwise = direction.rotateSteps(1);
+            this.offset = new Vector(offsetX, offsetY);
+        }
+
+        Vector location(Vector cell) {
+            return cell.plus(offset);
+        }
+
+        boolean matches(Set<Direction> externalNeighbours) {
+            boolean hasDirection = externalNeighbours.contains(direction);
+            boolean hasAnticlockwise = externalNeighbours.contains(anticlockwise);
+            boolean hasClockwise = externalNeighbours.contains(clockwise);
+
+            boolean outerCorner = hasAnticlockwise && hasClockwise;
+            boolean innerCorner = hasDirection && !hasAnticlockwise && !hasClockwise;
+
+            return outerCorner || innerCorner;
         }
     }
 }
--- src/test/java/uk/zootm/aoc2024/day12/Day12Test.java
+++ src/test/java/uk/zootm/aoc2024/day12/Day12Test.java
@@ -1,16 +1,18 @@
 package uk.zootm.aoc2024.day12;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.common.base.Preconditions;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
+import uk.zootm.aoc2024.day12.Day12.Corner;
 import uk.zootm.aoc2024.day12.Day12.Segment;
 import uk.zootm.aoc2024.grid.CharacterGrid;
 import uk.zootm.aoc2024.grid.Vector;
 
-import java.util.Set;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class Day12Test {
-    private static final String EXAMPLE = """
+    private static final String EXAMPLE =
+            """
             RRRRIICCFF
             RRRRIICCCF
             VVRRRCCFFF
@@ -28,11 +30,9 @@ public class Day12Test {
         var grid = CharacterGrid.fromString(EXAMPLE);
 
         // One of the smallest ones because I'm going to lose my mind otherwise
-        assertThat(Day12.findSegment(grid, new Vector(4, 0))).isEqualTo(new Segment("I", Set.of(
-                new Vector(4, 0),
-                new Vector(5, 0),
-                new Vector(4, 1),
-                new Vector(5, 1))));
+        assertThat(Day12.findSegment(grid, new Vector(4, 0)))
+                .isEqualTo(new Segment(
+                        "I", Set.of(new Vector(4, 0), new Vector(5, 0), new Vector(4, 1), new Vector(5, 1))));
     }
 
     @Test
@@ -40,23 +40,136 @@ public class Day12Test {
         var grid = CharacterGrid.fromString(EXAMPLE);
 
         // One of the smallest ones because I'm going to lose my mind otherwise
-        assertThat(Day12.findSegments(grid).stream().map(ResultParts::from)).containsExactlyInAnyOrder(
-                new ResultParts("R", 12, 18),
-                new ResultParts("I", 4, 8),
-                new ResultParts("C", 14, 28),
-                new ResultParts("F", 10, 18),
-                new ResultParts("V", 13, 20),
-                new ResultParts("J", 11, 20),
-                new ResultParts("C", 1, 4),
-                new ResultParts("E", 13, 18),
-                new ResultParts("I", 14, 22),
-                new ResultParts("M", 5, 12),
-                new ResultParts("S", 3, 8));
+        assertThat(Day12.findSegments(grid).stream().map(ResultParts::from))
+                .containsExactlyInAnyOrder(
+                        new ResultParts("R", 12, 18),
+                        new ResultParts("I", 4, 8),
+                        new ResultParts("C", 14, 28),
+                        new ResultParts("F", 10, 18),
+                        new ResultParts("V", 13, 20),
+                        new ResultParts("J", 11, 20),
+                        new ResultParts("C", 1, 4),
+                        new ResultParts("E", 13, 18),
+                        new ResultParts("I", 14, 22),
+                        new ResultParts("M", 5, 12),
+                        new ResultParts("S", 3, 8));
+    }
+
+    @Test
+    public void findCornersForCell_variousExamples() {
+        assertCornersAre(
+                """
+                OOO
+                OXO
+                OOO
+                """,
+                Corner.NE,
+                Corner.NW,
+                Corner.SW,
+                Corner.SE);
+
+        assertCornersAre(
+                """
+                OXO
+                XXX
+                OXO
+                """,
+                Corner.NE,
+                Corner.NW,
+                Corner.SW,
+                Corner.SE);
+
+        assertCornersAre(
+                """
+                XOX
+                OXO
+                XOX
+                """,
+                Corner.NE,
+                Corner.NW,
+                Corner.SW,
+                Corner.SE);
+
+        assertCornersAre(
+                """
+                XOO
+                OXO
+                OOO
+                """,
+                Corner.NE,
+                Corner.NW,
+                Corner.SW,
+                Corner.SE);
+
+        assertCornersAre(
+                """
+                OXO
+                OXO
+                OOO
+                """,
+                Corner.SW,
+                Corner.SE);
+
+        assertCornersAre(
+                """
+                XXX
+                XXO
+                OOO
+                """, Corner.SE);
+    }
+
+    @Test
+    public void doubleCorner() {
+        CharacterGrid grid = CharacterGrid.fromString(
+                """
+                        XXO
+                        XOX
+                        XXX
+                        """);
+
+        Segment x = Day12.findSegment(grid, new Vector(0, 0));
+
+        assertThat(x.cornerLocations()).contains(new Vector(2, 1), new Vector(2, 1));
+    }
+
+    private void assertCornersAre(String gridString, Corner... corner) {
+        CharacterGrid grid = CharacterGrid.fromString(gridString);
+
+        Preconditions.checkArgument(grid.height() == 3 && grid.width() == 3);
+
+        Segment segment = Day12.findSegment(grid, new Vector(1, 1));
+
+        assertThat(segment.corners(new Vector(1, 1))).containsExactlyInAnyOrder(corner);
+    }
+
+    @Test
+    public void findSegmentsWithDiscount_fromExample() {
+        var grid = CharacterGrid.fromString(EXAMPLE);
+
+        // One of the smallest ones because I'm going to lose my mind otherwise
+        assertThat(Day12.findSegments(grid).stream().map(ResultParts::fromWithDiscount))
+                .containsExactlyInAnyOrder(
+                        new ResultParts("R", 12, 10),
+                        new ResultParts("I", 4, 4),
+                        new ResultParts("C", 14, 22),
+                        new ResultParts("F", 10, 12),
+                        new ResultParts("V", 13, 10),
+                        new ResultParts("J", 11, 12),
+                        new ResultParts("C", 1, 4),
+                        new ResultParts("E", 13, 8),
+                        new ResultParts("I", 14, 16),
+                        new ResultParts("M", 5, 6),
+                        new ResultParts("S", 3, 6));
     }
 
     record ResultParts(String element, long area, long perimeter) {
         static ResultParts from(Segment segment) {
             return new ResultParts(segment.element(), segment.area(), segment.perimeter());
+        }
+
+        static ResultParts fromWithDiscount(Segment segment) {
+            return new ResultParts(
+                    segment.element(), segment.area(), segment.cornerLocations().count());
         }
     }
 }
